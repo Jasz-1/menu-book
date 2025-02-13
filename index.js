308cc3c7c58619bca4de38d4c1bfcc6c45948cfc
Jason <bravemafia001@gmail.com>
2025-02-13 15:56:31 +0700

    use 2 qty

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let qty = 0;
+let qty2 = 0;
 
 function addQty() {
     qty=qty+1;
@@ -9,4 +10,14 @@ function addQty() {
 function substractQty() {
     qty=qty-1;
     document.getElementById("qty").innerHTML = qty;
+}
+
+function addQty2() {
+    qty2=qty2+1;
+    document.getElementById("qty2").innerHTML = qty2; 
+}
+
+function substractQty2() {
+    qty2=qty2-1;
+    document.getElementById("qty2").innerHTML = qty2;
 }
