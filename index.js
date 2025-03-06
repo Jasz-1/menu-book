48eae31c5eb0913d1ec4f0585ddba39ffe5ae18f
Jason <bravemafia001@gmail.com>
2025-03-06 15:20:54 +0700

    create loop

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menus = [{
     title : "Avocado Pesto Salad",
-    photoUrl  : "https://www.google.com",
+    photoUrl  : "salad.png",
     description : "Home-grown vegetables with loads of avocado, topped with mouthwatering pesto salad",
     prices  : [{
     label : "",
@@ -11,7 +11,7 @@ const menus = [{
     }]
   },{
     title : "Triple Cheese Pizza",
-    photoUrl  : "https://www.google.com",
+    photoUrl  : "pizza.png",
     description : "Indulge yourself in this simple yet delicious delicacy",
     prices  : [{
     label : "1 Slice",
@@ -23,7 +23,7 @@ const menus = [{
   },{
 
     title : "Seasonal Wine",
-    photoUrl  : "https://www.google.com",
+    photoUrl  : "wine.png",
     description : "A great meal becomes perfect when combined with a good wine",
     prices  : [{
     label : "Glass",
@@ -35,7 +35,7 @@ const menus = [{
   },{
 
     title : "Coffee",
-    photoUrl  : "https://www.google.com",
+    photoUrl  : "coffee.png",
     description : "The best coffee you can have from the land of Indonesia",
     prices  : [{
     label : "Americano",
@@ -52,6 +52,56 @@ const cart = [
     [0, 0],
     [0, 0]
 ];
+
+let menuArea = "";
+for (let i = 0; i < menus.length; i++) {
+  const e = menus[i];
+
+  menuArea += `
+  <div class="menu-tile">
+  <div class="menu-photo">
+      <img src="${e.photoUrl}" alt="${e.title}">
+  </div>
+  <div class="menu-tile-name">${e.title}</div>
+  <div class="menu-tile-description">${e.description} </div>
+
+
+
+  <div class="menu-price-row">
+      <span class="price-description"></span>
+      <div class="price-and-qty">
+          <span class="price">35</span>
+          <button type="button" onclick='substractQty(0,0)'>
+              <img src="minus.png" width="16" height="16" alt="minus">
+          </button>
+          <span id="qty00" class="qty">0</span>
+          <button type="button" onclick='addQty(0,0)'>
+              <img src="plus.png" width="16" height="16" alt="plus">
+          </button>
+      </div>
+     
+  </div>
+
+
+
+  <div class="menu-price-row">
+      <span class="price-description">+ Grilled Salmon</span>
+      <div class="price-and-qty">
+      <span class="price">55</span>
+      
+      
+          <button type="button" onclick='substractQty(0,1)'>
+              <img src="minus.png" width="16" height="16" alt="minus">
+          </button>
+          <span id="qty01" class="qty">0</span>
+          <button type="button" onclick='addQty(0,1)'>
+              <img src="plus.png" width="16" height="16" alt="plus">
+          </button>
+      </div>
+  </div>
+</div> `
+
+}
 
 function addQty(menuIndex, priceIndex) {
 cart[menuIndex][priceIndex]=cart[menuIndex][priceIndex] + 1
