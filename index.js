3f4d6c8f0c59bfaebb113e2ac3393c1659e40369
Jason <bravemafia001@gmail.com>
2025-04-24 15:58:24 +0700

    reset display and data after button

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let totalPrice = 0
+let cart = [];
+
+
+
+
 
 const menus = [{
   title: "Avocado Pesto Salad",
@@ -46,7 +51,9 @@ const menus = [{
   }]
 }];
 
-const cart = [];
+
+reset();
+
 
 for (let i = 0; i < menus.length; i++) {
     cart.push([0,0]);
@@ -106,3 +113,60 @@ function substractQty(menuIndex, priceIndex) {
     document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart[menuIndex][priceIndex];
   }}
 
+function checkout(){
+    if(totalPrice>0) {
+    alert("Terimakasih atas pesanan anda: Rp " + (totalPrice*1000).toLocaleString());
+    reset()
+    } 
+    else{
+        alert("Pesan dulu minimal 1")
+    }
+}
+
+function reset(){
+    totalPrice=0;
+    cart = [];
+    document.getElementById("checkout").innerHTML = "Rp.0";
+    for (let i = 0; i < menus.length; i++) {
+        cart.push([0,0]);
+    }
+    
+    let menuArea = "";
+    
+    for (let i = 0; i < menus.length; i++) {
+      const e = menus[i];
+      let menuPrices = "";
+    
+      for (let j = 0; j < e.prices.length; j++) {
+          const f = e.prices[j];
+          menuPrices += `
+              <div class="menu-price-row">
+                  <span class="price-description">${f.label}</span>
+                  <div class="price-and-qty">
+                      <span class="price">${f.price}</span>
+                      <button onclick='substractQty(${i},${j})'>
+                          <img src="minus.png" width="16" height="16" alt="minus-circle" />
+                      </button>
+                      <span class="qty" id="qty${i}${j}">0</span>
+                      <button onclick='addQty(${i},${j})'>
+                          <img src="plus.png" width="16" height="16" alt="plus-circle" />
+                      </button>
+                  </div>
+              </div>
+          `;
+      }
+    
+      menuArea += `
+          <div class="menu-tile">
+              <div class="menu-photo">
+                  <img src="${e.photoUrl}" alt="${e.title}">
+              </div>
+              <div class="menu-tile-name">${e.title}</div>
+              <div class="menu-tile-description">${e.description}</div>
+              ${menuPrices}
+          </div>
+      `;
+    }
+    
+    document.getElementById("menu-area").innerHTML = menuArea;
+}
